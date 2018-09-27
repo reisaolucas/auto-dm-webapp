b4a215219a6b5ca1a0e88e903fff7fa1f9c1fc3b
Lucas Reis <reisaolucas@gmail.com>
2018-09-27 00:49:07 -0300

    First commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'autodm-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
